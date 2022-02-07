3ddb6930c1ad9d0cb84f2b405f06589ee0f811f8
tushar1003 <91830386+tushar1003@users.noreply.github.com>
2022-02-07 08:15:13 +0530

    drum website

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    var buttonInnerHtml = this.innerHTML;
+    makesound(buttonInnerHtml);
+    highlight(buttonInnerHtml);
+
+    //  var audio=new Audio("sounds/tom-1.mp3");
+    //  audio.play();
+  });
+  document.addEventListener('keypress',function(event){
+      makesound(event.key);
+      highlight(event.key);
+  });
+  function makesound(key){
+    switch (key) {
+        case 'w':
+          var audio = new Audio('sounds/tom-1.mp3');
+          audio.play();
+          break;
+        case 'a':
+          var audio = new Audio('sounds/tom-2.mp3');
+          audio.play();
+          break;
+        case 's':
+          var audio = new Audio('sounds/tom-3.mp3');
+          audio.play();
+          break;
+        case 'd':
+          var audio = new Audio('sounds/tom-4.mp3');
+          audio.play();
+          break;
+        case 'j':
+          var audio = new Audio('sounds/crash.mp3');
+          audio.play();
+          break;
+        case 'k':
+          var audio = new Audio('sounds/kick-bass.mp3');
+          audio.play();
+          break;
+        case 'l':
+          var audio = new Audio('sounds/snare.mp3');
+          audio.play();
+          break;
+      }
+
+  }
+  function highlight(key)
+  {
+      document.querySelector("."+ key).classList.add("pressed");
+      setTimeout(function(){
+      document.querySelector("."+ key).classList.remove("pressed");
+
+      },100);
+  }
+}
